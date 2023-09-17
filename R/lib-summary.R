03ef1497bf470be4ec3308278e3a3d99d6bb7226
Aayushi Verma <aayushigeekgirl@gmail.com>
2023-09-17 16:03:31 -0500

    bugfix

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -7,13 +7,16 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function(sizes) {
+lib_summary <- function(sizes=FALSE) {
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be either TRUE or FALSE. L.")
+  }
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors=FALSE)
   names(pkg_df) <- c("Library", "n_packages")
 
-  if (sizes) {
+  if (sizes == TRUE) {
     pkg_df$lib_size <- sapply(
       pkg_df$Library,
       function(x) {
